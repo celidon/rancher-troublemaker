921807b0e76b53392ccbbb994a46a38a91eb249c
Tony Hansen <tony.hansen@suse.com>
2024-11-20 15:55:34 -0600

    Problem and Check 4

--- scripts/checks/4.sh
+++ scripts/checks/4.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
-
-echo Solution 4
 MainDir=asdf
+RANCHER_URL=$(grep url $MainDir/connection_info | cut -d'"' -f2)
+RKC=$MainDir/tf/aws/kube_config_server.yaml
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if curl -o /dev/null -ILksf $RANCHER_URL; then
+  echo -e "$GREEN[PASS]$NC Rancher URL is up and responding"
+else
+  echo -e "$RED[FAIL]$NC Rancher URL is not returning 200 OK"
+  echo -e "$RED Remaining checks skipped $NC"
+  exit 1
+fi
+
+helm --kubeconfig $RKC get values -n cattle-system rancher 2>/dev/null > values.yaml
+CPU=$(grep cpu: values.yaml | cut -d: -f2 | tr -d " ")
+MEM=$(grep memory: values.yaml | cut -d: -f2 | tr -d " ")
+
+rm values.yaml
+
+if [ $CPU != 1 ] || [ $MEM != "1G" ]; then
+  echo -e "$RED[FAIL]$NC Resources not set correctly"
+  echo -e "$RED Remaining checks skipped $NC"
+  exit 1
+else
+  echo -e "$GREEN[PASS]$NC Resources set correctly"
+fi
+
+VER=$(helm get metadata rancher -n cattle-system 2>/dev/null | grep ^VERSION | cut -d" " -f2)
+
+if [ $VER != "2.9.3" ]; then
+  echo -e "$RED[FAIL]$NC Rancher version change"
+  exit 1
+else
+  echo -e "$GREEN[PASS]$NC Rancher version set correctly"
+fi
--- scripts/problems/4.sh
+++ scripts/problems/4.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 #High Requests
 
-
-echo Issue 4
 MainDir=asdf
+RKC=$MainDir/tf/aws/kube_config_server.yaml
+
+helm --kubeconfig $RKC get values -n cattle-system rancher 2>/dev/null > values.yaml
+cat >> values.yaml << EOF
+resources:
+  requests:
+    cpu: 11111
+    memory: 11111G
+EOF
+
+helm repo add rancher-prime https://charts.rancher.com/server-charts/prime &>/dev/null
+helm repo update &>/dev/null
+helm --kubeconfig $RKC upgrade rancher rancher-prime/rancher -n cattle-system -f values.yaml --version 2.9.3 &>/dev/null
+
+kubectl --kubeconfig $RKC patch -n cattle-system deployments.apps rancher --type=json -p='[{"op": "replace", "path": "/spec/strategy/type", "value":"Recreate"}, {"op": "remove", "path": "/spec/strategy/rollingUpdate"}]' &>/dev/null
+
+rm values.yaml
+
+echo "Rancher isn't running. Please get it back online"
+
+read -p "Would you like a hint? (y/n) " hint
+
+if [ $hint == y ] || [ $hint == Y ]; then
+    echo "I think I messed up editing my Helm values. I was trying to add resource requests of 1 CPU and 1G Memory."
+fi
