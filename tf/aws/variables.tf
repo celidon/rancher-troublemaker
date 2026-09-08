dbe77043f99ce929aaffdc831455789d1e12994e
Tony Hansen <tony.hansen@suse.com>
2026-09-08 10:02:11 -0500

    Update Rancher Version

--- tf/aws/variables.tf
+++ tf/aws/variables.tf
@@ -63,7 +63,7 @@ variable "cert_manager_version" {
 variable "rancher_version" {
   type        = string
   description = "Rancher server version (format: v0.0.0)"
-  default     = "2.12.4"
+  default     = "2.14.4"
 }
 
 variable "rancher_helm_repository" {
