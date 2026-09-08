56fdbc5cec7614230761304e71c78237c688bec2
Tony Hansen <tony.hansen@suse.com>
2026-09-08 16:22:05 -0500

    Changes to test 3

--- scripts/problems/3.sh
+++ scripts/problems/3.sh
@@ -7,13 +7,17 @@ IP=$(grep workload_node_ip $MainDir/connection_info | cut -d'"' -f2)
 SSH="ssh ec2-user@$IP -i $MainDir/id_rsa -o StrictHostKeyChecking=no -C sudo"
 
 $SSH touch /tmp/test
-$SSH 'cat > test.sh' <<< 'while true; do if [ -f /tmp/test ]; then rm /tmp/test; fallocate -l $(( $( stat -f --printf="%a * %s" / ) / 100 * 98 )) /tmp/test; else exit; fi; sleep 10; done'
+$SSH 'cat > test.sh' <<< 'while true; do if [ $(df --output=pcent / | tail -1 | cut -d% -f1) -lt 95 ]; then if [ -f /tmp/test ]; then rm /tmp/test; fallocate -l $(( $( stat -f --printf="%a * %s" / ) / 100 * 95 )) /tmp/test; else exit; fi; sleep 10; fi; done'
 $SSH bash test.sh &
+$SSH mkdir .kube
+$SSH cp /etc/rancher/rke2/rke2.yaml ./.kube/config
+$SSH chown -R ec2-user. .kube
+$SSH 'cat > .bashrc' <<< 'export PATH=$PATH:/var/lib/rancher/rke2/bin'
 
 echo "Downstream cluster went offline. Please investigate why and bring it back online."
 
 read -p "Would you like a hint? (y/n) " hint
 
 if [ $hint == y ] || [ $hint == Y ]; then
-    echo "You might need to delete something to relieve the pressure"
+    echo "You need to relieve the pressure"
 fi
